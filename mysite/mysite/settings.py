d13c53c907463db9f4b602c87c5bcb2113139176
amisalem91993 <amisalem91993@gmail.com>
2025-09-15 13:33:29 +0200

    added static manually

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,11 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
