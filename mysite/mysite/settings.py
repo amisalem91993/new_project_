dfce77da5b2663a835982eb409d63db45768dac1
amisalem91993 <amisalem91993@gmail.com>
2025-09-15 13:28:05 +0200

    added static manually

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,11 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#added manually
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     "/var/www/static/",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
